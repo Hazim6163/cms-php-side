11b115676d2bc276686b74608db8425bdc499be9
Mustafa <hazim6163@gmail.com>
2019-09-24 02:16:00 +0200

    1. update confitmation opration 2. create the confirmation form

--- validation/signup.php
+++ validation/signup.php
@@ -1,5 +1,11 @@
 <?php
 
+$title = 'Confirm Account';
+$costumeCss = '
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+
+';
+
 // check if the token is available :
 session_start();
 if(!isset($_SESSION['token'])){
@@ -20,13 +26,18 @@ $postFields = array(
     'token' => $token
 );
 
+$vKey;
+$email;
+$fname;
+$userFounded = false;
+
 if(isset($_POST['submit'])){
 
     //get the vKey from the user: 
         $uVKey = $_POST['vKey'];
 
     //send the post request to the server ->
-        $url = 'localhost:3000/users/getVKey';
+        $url = 'http://localhost:3000/users/getVKey';
         $options = array(
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_POST => 1,
@@ -44,22 +55,22 @@ if(isset($_POST['submit'])){
 
     if($responseCode != 200){
         //echo('there is an error');
-    }
+    }else{
+        $userFounded = true;
+        //catch the vKey and Email from the server 
+        $vKey = $result->vKey;
+        $email = $result->email;
+        $fname = $result->fname;
 
-    //catch the vKey and Email from the server 
-    //$vKey = $result->vKey;
-    //$email = $result->email;
-    //$fname = $result->fname;
-    $vKey = 123456;
-
-    //compare the vKeys:
-    if($vKey == $uVKey){
-        //conformation successfully: 
-            $_SESSION['verified'] = 1 ;
-            header('Location: ../index.php');
-    }else {
-        //confirmation failed:
-            $_SESSION['error'] = 'Wrong confirmation Code';
+        //compare the vKeys:
+        if($vKey == $uVKey){
+            //conformation successfully: 
+                $_SESSION['verified'] = 1 ;
+                header('Location: ../index.php');
+        }else {
+            //confirmation failed:
+                $_SESSION['error'] = 'Wrong confirmation Code';
+        }
     }
 }
 
@@ -69,3 +80,83 @@ include('../include/navbar.php');
 ?>
 
 <!-- create confirmation form -->
+<style>
+  
+  .container{
+        font-family: 'Open Sans';
+    }
+
+    .content{
+        text-align: center;
+        padding-left: 36px;
+    }
+
+    .title{
+        box-sizing: border-box;
+        width:100%;
+        text-align: center;
+        padding-bottom: 36px;
+        color: #a74165;
+    }
+
+    .inputGroup-6 > .inputDescription {
+        text-align: left;
+        padding-left: 8px;
+    }
+
+    .vKey-input{
+        appearance: none;
+    }
+
+    input[type='number'] {
+        -moz-appearance:textfield;
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+    }
+
+    #submitFormBtn{
+        margin-top: 16px;
+    }
+
+    .inputGroup-6 {
+        margin-bottom: 0px;
+    }
+
+    .invalidEntry {
+        margin:16px 0px 0px 0px;
+    }
+
+</style>
+
+<?php if($userFounded){ ?>
+    <div class="container">
+        <h1 class="title">Welcome <?php echo($fname) ?> !!</h1>
+        <div class="content">
+            <p>we already sent you the confirmation code to your E-mail: <b><?php echo($email) ?></b>.</p>
+            <p style="padding: 16px;">please enter the confirmation Code:</p>
+            <form action="" method="post">
+                <div class="inputGroup-6 vKey-input">
+                    <div class="inputDescription">vKey: </div>
+                    <input name="vKey" type="number">
+                </div>
+                <?php if (isset($_SESSION['error'])){ ?>
+                    <div class="invalidEntry">
+                        <span><?php echo $_SESSION['error']; ?></span>
+                    </div>
+                <?php } ?>
+                <input type="submit" id="submitFormBtn" name="submit" value="confirm">
+            </form>
+        </div>
+    </div>
+<?php 
+}
+else{
+    /* if the user not founded on the server or was server error
+    unset($_SESSION('token')); //to redirect the user to the login page
+    header('Location: ../login.php');
+    */
+} 
+?>
