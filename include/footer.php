40d9b8906091a58902f8fa59664760b49d385893
Mustafa <hazim6163@gmail.com>
2019-10-04 10:35:50 +0200

    create site footer change nav color to black

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,13 @@
+
+<section class="footer">
+    <div class="footerBrand">CMS</div>
+    <div class="footerContent">
+        &copy; All copyright received <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        Hazim6163
+    </div>
+</section>
+
 </body>
 
 </html>
