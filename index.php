6d380b984038c7f075b79693aef600a9fce9c16c
Mustafa <hazim6163@gmail.com>
2019-10-08 16:59:17 +0200

    start working on the home page

--- index.php
+++ index.php
@@ -1,63 +1,23 @@
 <?php
-require ('./Classes/Category.php');
-require ('./Classes/Post.php');
 
-$costumeCss = '
+$title = 'Home';
+$custom_head = '
 <link rel="stylesheet" href="./assets/css/index.css">
+<script src="https://kit.fontawesome.com/9b9c3e7d62.js" crossorigin="anonymous"></script>
 ';
 
-//array we need in home page 
-$rootCategories = Category::getRootCategories();
-$lastPosts = Post::getLastPosts();
-
 session_start();
+
 include('./include/header.php');
 include('./include/navbar.php');
-//include('/include/home/title');
-
-/**categories access: */
-if(!$rootCategories['error']){
-    $rootCategories = $rootCategories['result'];
-}else{
-    session_start();
-    $_SESSION['errorCode'] = 600;
-    header('Location: ./error.php');
-}
-
-echo('
-    <h1>Categories</h1><br>
-');
-foreach ($rootCategories as $category) {
-    /**echo('
-        title: '.$category['title'].' <br>
-        des: '.$category['des'].' <br>
-        created: '.$category['createdAt'].' <br>
-    ');*/
-
-}
-
-/**last post array */
-if (!$lastPosts['error']) {
-    $lastPosts = $lastPosts['result'];
-}else{
-    session_start();
-    $_SESSION['errorCode'] = 601;
-    session_write_close();
-    header('Location: ./error.php');
-}
-echo('<br><br><h1>Last posts: </h1><br>');
-foreach ($lastPosts as $post) {
-   /** 
-    * echo('
-    *    title: '.$post['title'].'<br>
-    *    des: '.$post['des'].'<br>
-    *    body: '.$post['body'].'
-    *    <br><br>');  
-    */ 
-}
-include('./include/home/search.php');
 ?>
 
+<!-- main container-->
+<div class="container">
+    <div class="searchBar">
+        <?php include('./include/home/search.php') ?>
+    </div>
+</div>
 
 <?php 
 include('./include/footer.php');
