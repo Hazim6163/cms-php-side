215730be17fcc4b9f4bcd717f246bc4a7eb6d064
Mustafa <hazim6163@gmail.com>
2019-10-10 20:21:53 +0200

    create post layout in js

--- assets/js/home.js
+++ assets/js/home.js
@@ -11,11 +11,89 @@ var userLoggedIn = false;
 var userInfo = getUserInfo();
 
 
-$(document).ready(()=>{
-   log('user info', userInfo.id);
-});
+//get posts container:
+const postsContainer = $('#p_postsContainer');
+
+//attach the post item to posts container:
+$.post('./include/home/posts.php', {lastPosts: true}, (lastPosts)=>{
+    //loop throw each post
+    lastPosts.forEach(post => {
+        postsContainer.append(createPost(post));
+    });
+}, 'json')
+
 
 /*---------------------- support methods : ---------------------*/
+
+//create post function take post data as parameter:
+function createPost(post){
+    //create post container:
+    var postContainer = $('<div>',{
+        id: post._id,
+        class: 'post'
+    });
+    // post header
+    postHeader = getPostHeader(post);
+    postHeader.appendTo(postContainer);
+    // post body
+    postBody = getPostBody(post);
+    postBody.appendTo(postContainer);
+    // post footer
+    postFooter = getPostFooter(post);
+    postFooter.appendTo(postContainer);
+    // post comments
+    postComments = getPostComments(post);
+    postComments.appendTo(postContainer);
+    //create on Actions reactions:
+    postContainer = onAction(post, postContainer);
+
+    return postContainer;
+}
+
+//create post header
+function getPostHeader(post){
+    const postHeader = $('<div>',{
+        id: 'postHeader'+post._id,
+        class: 'postHeader'
+    });
+
+    return postHeader;
+}
+
+//create post body
+function getPostBody(post) {
+    const postBody = $('<div>',{
+        id: 'postBody'+post._id,
+        class: 'postBody'
+    });
+
+    return postBody;
+}
+
+//create post footer
+function getPostFooter(post){
+    const postFooter = $('<div>',{
+        id: 'postFooter'+post._id,
+        class: 'postFooter'
+    });
+
+    return postFooter;
+}
+
+//create post comments
+function getPostComments(post){
+    const postComments = $('<div>',{
+        id: 'postComments'+post._id,
+        class: 'postComments'
+    });
+
+    return postComments;
+}
+
+//reactions ...
+function onAction(post, postContainer){
+    return postContainer;
+}
 
 //log message:
 function log(des, msg) {
@@ -32,7 +110,7 @@ function log(des, msg) {
 function getUserInfo(){
     var userInfo;
     $.ajax({
-        async: false,
+        async: true,
         type:'POST',
         url: './include/home/posts.php',
         data: {userInformation: true},
