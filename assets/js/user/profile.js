d67ce33aab15028897c7d959a0614d6a0f369534
Mustafa <hazim6163@gmail.com>
2019-10-29 16:16:46 +0100

    create post class

--- assets/js/user/profile.js
+++ assets/js/user/profile.js
@@ -1,26 +1,408 @@
-/**
- * page will show the user posts
- * page would be divided by date year month  
- * 
- * todo: 
- * get data from php that's include the user posts
- * post expected data:
- *  authorInfo 
- *  post body, title, des, likes, comments, replays, date, categories to the parent cat, tags
- *  
- * create the custom post card put it inside user posts date range
- * 
- * */
 
- //get data: 
- const getData = (nextFun) =>{
-   $.post('./profile.php', {getData: true}, (data) => {
+//vars: 
+//url params : 
+const params = new URLSearchParams(window.location.search);
+//requested user id: 
+const rId = params.get('id');
+//user login status boolean
+let userLoggedIn = false;
+
+//get user data: 
+getUserInfo((userInfo) => {
+    (userInfo.loggedIn == false) ? userLoggedIn = false : userLoggedIn = true;
+    //get data:
+    getData((data) => {
+        //inflate page:
+        inflatePage(data, userInfo);
+    })
+});
+
+//get data: 
+const getData = (nextFun) => {
+    //create data url: 
+    let path;
+    rId == null ? path = './profile.php' : path = './profile.php?id=' + rId;
+    $.post(path, { getData: true }, (data) => {
         nextFun(data);
-   }, 'json')
+    }, 'json')
 }
 
-getData(createPage);
+function inflatePage(data, userInfo) {
+    const links = {
+        catLink: '../categories.php?id=',
+        profileLink: './profile.php?id=',
+        tagLink: '', //todo create tag link
+        authorImgLink: 'http://localhost:3000/user/profilePhoto?id=',
+        phpUtils: './profile.php'
+    }
 
-function createPage(data){
-    console.log(data);
+    //get page container: 
+    const pageContainer = $('#pageContainer');
+    const post = new Post(data[0], links, userInfo);
+    post.createTags();
+    pageContainer.append(post.createCatTree());
+
+}
+
+
+//function to create post component
+class Post {
+    constructor(post, links, userInfo) {
+        //operation vars: 
+        this.authorMenuInProgress = false;
+        //links:
+        this.catLink = links.catLink;
+        this.tagLink = links.tagLink;
+        this.profileLink = links.profileLink;
+        this.phpUtils = links.phpUtils;
+        //post vars:
+        this.parentId = post.parentId;
+        this.authorId = post.authorId;
+        this.authorInfo = post.authorInfo;
+        this.catTree = post.catTree;
+        this.id = post._id;
+        this.title = post.title;
+        this.des = post.des;
+        this.body = post.body;
+        post.img ? this.img = post.img : this.img = undefined;
+        this.tags = post.tags;
+        this.likesCount = post.likesCount;
+        this.likers = post.likers;
+        this.commentsCount = post.commentsCount;
+        this.comments = post.comments;
+        this.createdAt = post.createdAt;
+        this.updatedAt = post.updatedAt;
+        //create post title element: 
+        this.eTitle = eHtml({ class: 'postTitle', html: this.title });
+        //create post des element: 
+        this.eDes = eHtml({ class: 'postDes', html: this.des });
+        //create post body element: 
+        this.eBody = eHtml({ class: 'postBody', html: this.body });
+        //create post created date element: 
+        this.eCreated = eHtml({ class: 'postCreated', html: this.createdAt });
+        //create post updated element: 
+        this.eUpdated = eHtml({ class: 'postUpdated', html: this.updatedAt });
+        //create post likes count element: 
+        this.eLikesCount = eHtml({ class: 'postLikesCount', html: this.likesCount });
+        //create post comments count element: 
+        this.eCommentsCount = eHtml({ class: 'postCommentsCount', html: this.commentsCount });
+        //create post author fname element: 
+        this.eAuthorFname = eHtml({ class: 'postAuthorFname', html: this.authorInfo.fname });
+        //create post author lname element: 
+        this.eAuthorLname = eHtml({ class: 'postAuthorLname', html: this.authorInfo.lname });
+        //create post author name: 
+        this.eAuthorName = eHtml({
+            class: 'postAuthorName',
+            html: this.authorInfo.fname + ' ' + this.authorInfo.lname
+        });
+        //create post author username element: 
+        this.eAuthorUsername = eHtml({ class: 'postAuthorUsername', html: this.authorInfo.username });
+
+
+    }
+
+    //post img:
+    postImg() {
+        const wrapper = eHtml({ class: 'pImgWrapper' });
+        const container = eHtml({ class: 'pImgContainer' });
+        //check if the post has img:
+        const url = this.img;
+        if (url && url != '' && url != null) {
+            //create post img
+            const img = eHtml({ type: 'img', class: 'postImg', container: container });
+            img.attr('src', this.links.postImg + url);
+        } else {
+            //create post icon: 
+            container.html('<i class="fas fa-image postIcon"></i>')
+        }
+        return wrapper;
+    }
+
+    //create tags wrapper
+    createTags() {
+        //create tags wrapper
+        const wrapper = eHtml({ class: 'tagsWrapper' });
+        //create tags container
+        const container = eHtml({ class: 'tagsContainer', container: wrapper });
+        //create tag container
+        this.tags.forEach(tag => {
+            const tContainer = eHtml({
+                class: 'tagContainer',
+                container: container,
+                onClick: () => {
+                    window.location.href = this.links.tagLink + tag._id
+                }
+            })
+            const icon = eHtml({ class: 'tagIconContainer', html: '<i class="fas fa-tag tagIcon"></i>', container: tContainer });
+            const name = eHtml({ class: 'tagName', html: tag.name, container: tContainer });
+        });
+        //todo test tags
+        return wrapper;
+    }
+
+    //create category tree wrapper: 
+    createCatTree() {
+        //tree wrapper: 
+        const wrapper = eHtml({ class: 'catTreeWrapper' });
+        //tree links container
+        const container = eHtml({ class: 'catTreeContainer', container: wrapper });
+        //home link
+        const home = eHtml({ class: 'body-nav', text: 'Home', container: container });
+        //slash
+        const slash = eHtml({ class: 'body-nav body-nav-slash', text: '/', container: container });
+        //categories link
+        const categories = eHtml({ class: 'body-nav', text: 'Categories', container: container });
+        //loop throw each parent: 
+        this.catTree.reverse().forEach((cat) => {
+            //append slash
+            container.append(slash.clone(true));
+            //create cat container: 
+            const cContainer = eHtml({
+                class: 'body-nav-cat body-nav',
+                text: cat.title,
+                container: container,
+                onClick: () => {
+                    window.location.href = this.catLink + cat._id
+                }
+            });
+        })
+        //todo test categories
+        return wrapper;
+    }
+
+    //author img:
+    creatAuthorImg() {
+        const wrapper = eHtml({ class: 'pAuthorImgWrapper' });
+        //img container: 
+        const container = eHtml({ class: 'pAuthorImgContainer', container: wrapper });
+        //check if the user has img: 
+        const url = this.authorInfo.photoUrl;
+        if (url && url != '' && url != null) {
+            //has photo
+            const img = htmlE({ type: 'img', class: 'authorImg', container: container });
+            img.attr('src', this.links.authorImgLink + this.authorInfo.photoUrl);
+        } else {
+            //create icon
+            container.html('<i class="fas fa-user userIcon"></i>')
+        }
+
+        return wrapper;
+    }
+
+    //post author menu: 
+    createAuthorMenu() {
+        const wrapper = eHtml({ class: 'authorMenuWrapper' });
+        const container = eHtml({ class: 'authorMenuContainer', container: wrapper });
+        const icon = eHtml({ class: 'authorMenuIconContainer menuCollapsed', id: 'authorMenuIconContainer', container: container, html: '<i class="fas fa-ellipsis-v menuIcon"></i>' });
+        const menu = eHtml({ class: 'authorMenu', id: 'authorMenu', container: container }).hide();
+        icon.click(() => {
+            menu.toggle('fast', () => {
+                //check menu status:
+                let displayed;
+                menu.is(":visible") ? displayed = true : displayed = false;
+                displayed ? icon.html('<i class="fas fa-times menuIcon"></i>') : icon.html('<i class="fas fa-ellipsis-v menuIcon"></i>')
+            });
+        })
+        const privacy = eHtml({ class: 'mPrivacy', id: 'mPrivacy', text: 'Privacy', container: menu });
+        privacy.click(() => {
+            if (this.authorMenuInProgress) return;
+            this.authorMenuInProgress = true;
+            this.createPrivacyModal();
+        })
+        const edit = eHtml({ class: 'mEditPost', id: 'mEditPost', text: 'Edit', container: menu });
+        edit.click(() => {
+            if (this.authorMenuInProgress) return;
+            window.location.href = window.location.href //todo create edit post page
+        })
+        const mDelete = eHtml({ class: 'mDelete', id: 'mDelete', text: 'Delete', container: menu });
+        mDelete.click(() => {
+            if (this.authorMenuInProgress) return;
+            this.authorMenuInProgress = true;
+            //change icon to in progress
+            icon.html('<i class="fas fa-spinner rotate"></i>');
+            //delete request:
+            $.post(this.phpUtils, { deletePost: true, id: this.id, in: 'profile', extra: { id: this.authorId } }, (res) => {
+                if (res.deleted) {
+                    //todo update posts container
+                    this.createAlertModal({
+                        message: 'Post Deleted !!', status: 'p', nextFun: () => {
+                            this.authorMenuInProgress = false;
+                        }
+                    });
+                } else {
+                    //todo create alert modal
+                    this.createAlertModal({
+                        message: res.error, status: 'n', nextFun: () => {
+                            this.authorMenuInProgress = false;
+                        }
+                    });
+                }
+                menu.hide('fast', () => {
+                    icon.html('<i class="fas fa-ellipsis-v menuIcon"></i>');
+                });
+            })
+        })
+        return wrapper;
+    }
+
+    //privacy modal: 
+    createPrivacyModal() {
+        //change icon to in progress
+        $('#authorMenuIconContainer').html('<i class="fas fa-spinner rotate"></i>');
+        const wrapper = eHtml({ class: 'modalWrapper' });
+        const container = eHtml({ class: 'privacyMContainer', id: 'privacyMContainer', container: wrapper });
+        const close = eHtml({ class: 'modalClose privacyMCloseContainer', id: 'closePrivacyM', container: container, html: '<i class="fas fa-times privacyMCloseIcon"></i>' });
+        close.click(() => {
+            wrapper.remove();
+            this.authorMenuInProgress = false;
+            $('#authorMenu').hide('fast', () => {
+                $('#authorMenuIconContainer').html('<i class="fas fa-ellipsis-v menuIcon"></i>');
+            });
+        });
+        //modal content
+        const content = eHtml({ class: 'privacyMContent', container: container });
+        //privacy dropdown label:
+        const label = eHtml({ class: 'privacyMLabel', text: 'Privacy : ', container: content });
+        //privacy dropdown
+        const dropdown = eHtml({ type: 'select', name: 'privacy', class: 'privacyDropdown', container: content });
+        //info : 
+        const info = eHtml({ class: 'privacyMInfo', container: content }).hide();
+        const infoIcon = eHtml({ class: 'privacy-modal-info-icon-container', container: info, html: '<i class="fas fa-info-circle privacy-modal-info-icon"></i>' });
+        const infoMsg = eHtml({ class: 'privacy-modal-info-msg', container: info });
+        //public option
+        const _public = eHtml({ type: 'option', value: 'public', text: 'Public', container: dropdown, class: 'privacyDropdownOption' });
+        //private option
+        const _private = eHtml({ type: 'option', value: 'private', text: 'Private', container: dropdown, class: 'privacyDropdownOption' });
+        //modal footer: 
+        const footer = eHtml({ class: 'privacyMFooter', container: container });
+        const save = eHtml({ class: 'privacyMSave', container: footer, text: 'Save' });
+        //on privacy save click:
+        save.click(() => {
+            const val = dropdown.children("option:selected").val();
+            $.post(this.phpUtils, { savePrivacy: true, privacy: val, id: this.id }, (res) => { //todo handel request
+                info.show('fast');
+                if (res.changed) {
+                    info.addClass('privacy-modal-success-info');
+                    infoMsg.text(res.message);
+                    setTimeout(() => {
+                        close.trigger('click');
+                        this.authorMenuInProgress = false;
+                    }, 2000)
+                } else {
+                    info.addClass('privacy-modal-failed-info');
+                    infoMsg.text(res.error);
+                }
+                $('#authorMenu').hide('fast', () => {
+                    $('#authorMenuIconContainer').html('<i class="fas fa-ellipsis-v menuIcon"></i>');
+                });
+            }, 'json')
+        });
+        $('body').append(wrapper);
+    }
+
+    //alerts modal: 
+    createAlertModal(data) {
+        //modal message
+        const message = data.message;
+        //status n: negative or p: positive or undefined
+        const status = data.status;
+        //modal wrapper
+        const wrapper = eHtml({ class: 'modalWrapper' });
+        //modal container
+        const container = eHtml({ class: 'alertModal', id: 'alertModal', container: wrapper });
+        //close btn
+        const close = eHtml({ class: 'modalClose alertMCloseContainer', id: 'closeAlertM', container: container, html: '<i class="fas fa-times alertMCloseIcon"></i>' });
+        close.click(() => {
+            wrapper.remove();
+        });
+        //modal content
+        const content = eHtml({ class: 'AlertMContent', container: container });
+        //info container
+        const info = eHtml({ class: 'alertMInfo', container: content });
+        //info icon
+        const infoIcon = eHtml({ class: 'alert-modal-info-icon-container', container: info, html: '<i class="fas fa-info-circle alert-modal-info-icon"></i>' });
+        //info message
+        const infoMsg = eHtml({ class: 'alert-modal-info-msg', container: info, text: message });
+        //add status classes
+        if (status) {
+            status == 'p' ? info.addClass('alert-info-success') : info.addClass('alert-info-failed');
+        } else {
+            info.addClass('alert-info');
+        }
+        //modal footer: 
+        const footer = eHtml({ class: 'alertMFooter', container: container });
+        const done = eHtml({ class: 'alertMDone', container: footer, text: 'Done' });
+        //on alert done click:
+        done.click(() => {
+            close.trigger('click');
+            data.nextFun(data.args);
+        });
+
+        $('body').append(wrapper);
+    }
+
+}
+
+class Comment {
+    constructor(data) {
+        this.comment = data.comment;
+        this.body = this.comment.body;
+        this.updatedAt = this.comment.updatedAt;
+        this.likesCount = this.comment.likesCount;
+        this.likers = this.comment.likers;
+        this.replaysCount = this.comment.replaysCount;
+        this.replays = this.comment.replays;
+        this.authorInfo = this.comment.authorInfo;
+    }
+}
+
+class Replay {
+    constructor(data) {
+        this.replay = data.replay;
+        this.body = this.replay.body;
+        this.updatedAt = this.replay.updatedAt;
+        this.likesCount = this.replay.likesCount;
+        this.authorInfo = this.replay.authorInfo;
+    }
+}
+
+//create html element:
+function eHtml(data) {
+    data.type ? e = $('<' + data.type + '>') : e = $('<div>');
+
+    if (data.class) {
+        e.addClass(data.class);
+    }
+
+    if (data.id) {
+        e.attr('id', data.id);
+    }
+
+    if (data.html) {
+        e.html(data.html);
+    }
+
+    if (data.text) {
+        e.text(data.text);
+    }
+
+    if (data.container) {
+        e.appendTo(data.container);
+    }
+
+    if (data.name) {
+        e.attr('name', data.name);
+    }
+
+    if (data.value) {
+        e.attr('value', data.value);
+    }
+
+    if (data.onClick) {
+        e.click(() => {
+            data.onClick(data.params)
+        });
+    }
+
+    return e;
 }
